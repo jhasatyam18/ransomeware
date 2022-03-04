91e35ad8447f57592dfe8d9b46ba3a14ac2c8401
shikha-max <tshikha66@mail.com>
2022-03-04 18:37:16 +0530

    Added unit testing framework
    I have used jest,react-testing-library,jest-html-reported
    
    GitHub issue id : #209
    Testing Done    : Tested DashboardEvents,ApiUtils,Generated code coverage report.

--- src/__tests__/tetsUtils/reduxRender.js
+++ src/__tests__/tetsUtils/reduxRender.js
@@ -4,13 +4,16 @@ import { Provider } from "react-redux";
 import React from "react";
 import store from "../../store";
 import {render } from '@testing-library/react';
-
+import i18n from '../../i18n';
+import { I18nextProvider } from 'react-i18next';
 
 export default function renderWitRedux(component){
   return { ...render(<Provider store={store}>
-    <BrowserRouter>
+   <I18nextProvider i18n={i18n}>
+   <BrowserRouter>
       {component}
     </BrowserRouter>
+   </I18nextProvider>
   </Provider>)}
 }
 
