b93a5dafd6f6fb3b25680f3e462a6e21464c50e2
shikha-max <tshikha66@mail.com>
2022-03-08 15:52:49 +0530

    Added unit testing framework I have used jest,react-testing-library,jest-html-reported
    GitHub issue id : #209
    Testing Done    : Tested DashboardEvents,ApiUtils,Generated code coverage report.

--- src/components/Dashboard/DashboardAlertOverview.js
+++ src/components/Dashboard/DashboardAlertOverview.js
@@ -1,69 +1,69 @@
-import React, { useEffect, useState } from "react"
-import { withTranslation } from "react-i18next"
-import { useDispatch, useSelector } from "react-redux"
-import { Link } from "react-router-dom"
-import { Card, CardBody, Col, Row } from "reactstrap"
-import { addMessage } from "../../store/actions/MessageActions"
-import { API_DASHBOARD_UNACK_ALERTS } from "../../constants/ApiConstants"
-import { ALERTS_PATH } from "../../constants/RouterConstants"
-import { MESSAGE_TYPES } from "../../constants/MessageConstants"
-import { callAPI } from "../../utils/ApiUtils"
-import Spinner from "../Common/Spinner"
+import React, { useEffect, useState } from 'react';
+import { withTranslation } from 'react-i18next';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Card, CardBody, Col, Row } from 'reactstrap';
+import { addMessage } from '../../store/actions/MessageActions';
+import { API_DASHBOARD_UNACK_ALERTS } from '../../constants/ApiConstants';
+import { ALERTS_PATH } from '../../constants/RouterConstants';
+import { MESSAGE_TYPES } from '../../constants/MessageConstants';
+import { callAPI } from '../../utils/ApiUtils';
+import Spinner from '../Common/Spinner';
 
 function DashboardAlertOverview(props) {
-  const dispatch = useDispatch()
-  const refresh = useSelector(state => state.user.context.refresh)
-  const [alert, setAlerts] = useState({criticalAlerts: 0,errorAlerts: 0,majorAlerts: 0,warningAlerts: 0,})
-  const [loading, setLoading] = useState(false)
+  const dispatch = useDispatch();
+  const refresh = useSelector((state) => state.user.context.refresh);
+  const [alert, setAlerts] = useState({ criticalAlerts: 0, errorAlerts: 0, majorAlerts: 0, warningAlerts: 0 });
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    let isUnmounting = false
-    setLoading(true)
-    setAlerts([])
+    let isUnmounting = false;
+    setLoading(true);
+    setAlerts([]);
     callAPI(API_DASHBOARD_UNACK_ALERTS).then(
-      json => {
-        if (isUnmounting) return
-        setAlerts(json)
-        setLoading(false)
+      (json) => {
+        if (isUnmounting) return;
+        setAlerts(json);
+        setLoading(false);
       },
-      err => {
-        setAlerts([])
-        setLoading(false)
-        dispatch(addMessage(err.message, MESSAGE_TYPES.ERROR))
-      }
-    )
+      (err) => {
+        setAlerts([]);
+        setLoading(false);
+        dispatch(addMessage(err.message, MESSAGE_TYPES.ERROR));
+      },
+    );
     return () => {
-      isUnmounting = true
-    }
-  }, [refresh])
-  const { t } = props
+      isUnmounting = true;
+    };
+  }, [refresh]);
+  const { t } = props;
 
   return (
     <Card>
       <CardBody style={{ maxHeight: 250 }}>
         <Link to={ALERTS_PATH} className="font-weight-medium text-white">
-          <p className="font-weight-medium text-muted">{t("Alerts")}</p>
-          <Row className="text-center" style={{ fontSize: "0.9rem" }}>
+          <p className="font-weight-medium text-muted">{t('Alerts')}</p>
+          <Row className="text-center" style={{ fontSize: '0.9rem' }}>
             <Col sm={4}>
-              <span className="text-danger-1">{t("Critical")}</span>
+              <span className="text-danger-1">{t('Alerts')}</span>
               <hr />
               {loading === true ? <Spinner /> : alert.criticalAlerts}
             </Col>
             <Col sm={4}>
-              <span className="text-danger-2">{t("Error")}</span>
+              <span className="text-danger-2">{t('Error')}</span>
               <hr />
-              {loading === true ? "" : alert.errorAlerts}
+              {loading === true ? '' : alert.errorAlerts}
             </Col>
             <Col sm={4}>
-              <span className="text-warning-1">{t("Major")}</span>
+              <span className="text-warning-1">{t('Major')}</span>
               <hr />
-              {loading === true ? "" : alert.majorAlerts}
+              {loading === true ? '' : alert.majorAlerts}
             </Col>
           </Row>
         </Link>
       </CardBody>
     </Card>
-  )
+  );
 }
 
-export default withTranslation()(DashboardAlertOverview)
+export default withTranslation()(DashboardAlertOverview);
