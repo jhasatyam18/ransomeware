9a770e0a229c9a9115e82b24cc12372c3e6b600e
shikha-max <tshikha66@gmail.com>
2022-05-16 10:28:21 +0530

    tests

--- src/__tests__/components/dashboard/protectedVsUnProtectedVMs.test.js
+++ src/__tests__/components/dashboard/protectedVsUnProtectedVMs.test.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { screen, cleanup } from '@testing-library/react';
+import { cleanup } from '@testing-library/react';
 import { setupServer } from 'msw/node';
 import ProtectedVsUnProtectedVMs from '../../../components/Dashboard/ProtectedVsUnProtectedVMs';
 import { API_DASHBOARD_VIRTUAL_MACHINE_PROTECTION_ANALYSIS_PROTECTED_VMS, API_DASHBOARD_REPLICATION_STATS } from '../../../constants/ApiConstants';
 import renderWitRedux from '../../tetsUtils/RenderWithRedux';
 import { withoutErrorResponse } from '../../tetsUtils/server';
 // API_RECOVERY_JOBS, withErrorResponse
-const replicationJobResp = { protectedVMs: 1, unprotectedVMs: 19 };
-const recoveredJobResp = { changedRate: 3.1607142857142856, completed: 8, dataReduction: 59.31, failed: 2, inSync: 1, notInsync: 0, rpo: 600, running: 0 };
-const jobReplicationResp = withoutErrorResponse(API_DASHBOARD_VIRTUAL_MACHINE_PROTECTION_ANALYSIS_PROTECTED_VMS, replicationJobResp);
-const jobRecoveryResp = withoutErrorResponse(API_DASHBOARD_REPLICATION_STATS, recoveredJobResp);
-const handlers = [jobRecoveryResp, jobReplicationResp];
+const vmAnalysisResp = [{ protectedVMs: 1, unprotectedVMs: 19 }];
+const replicationState = { changedRate: 3.1607142857142856, completed: 8, dataReduction: 59.31, failed: 2, inSync: 1, notInsync: 0, rpo: 600, running: 0 };
+const vmAnalysis = withoutErrorResponse(API_DASHBOARD_VIRTUAL_MACHINE_PROTECTION_ANALYSIS_PROTECTED_VMS, vmAnalysisResp);
+const replication = withoutErrorResponse(API_DASHBOARD_REPLICATION_STATS, replicationState);
+const handlers = [vmAnalysis, replication];
 const server = new setupServer(...handlers);
 describe('ProtectedVsUnProtectedVMs : Dashboard Job tests', () => {
   beforeAll(() => server.listen());
