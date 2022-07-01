c74e94a41ad9cbfd64c256065c2e1448272f3a42
shikha-max <tshikha66@gmail.com>
2022-07-01 16:38:52 +0530

    updated with commented changes

--- src/utils/InputUtils.js
+++ src/utils/InputUtils.js
@@ -828,7 +828,8 @@ export function getMatchingInsType(values, ins) {
 
 export function convertKBtoUnit(data) {
   const sizes = ['KB', 'MB', 'GB', 'TB'];
-  if (data === 0) return '0 KB';
+  if (data === 0) return '-';
   const i = parseInt(Math.floor(Math.log(data) / Math.log(1024)), 10);
+  if (i >= sizes.length) return '-';
   return `${Math.round(data / 1024 ** i, 2)} ${sizes[i]}`;
 }
