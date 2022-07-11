5bc99da2cb65d5e160238006f3c96c45e0b19a71
shikha-max <tshikha66@gmail.com>
2022-07-11 17:44:58 +0530

    added conditions in array

--- src/components/Table/ItemRenderers/StatusItemRenderer.js
+++ src/components/Table/ItemRenderers/StatusItemRenderer.js
@@ -4,6 +4,11 @@ import { NODE_STATUS_ONLINE, NODE_STATUS_OFFLINE, JOB_RECOVERED, JOB_COMPLETION_
 import 'boxicons';
 
 function StatusItemRenderer({ data, field }) {
+  const successArray = [JOB_COMPLETION_STATUS, JOB_INIT_SUCCESS, NODE_STATUS_ONLINE, JOB_RESYNC_SUCCESS, JOB_IN_SYNC, JOB_RECOVERED, 'migrated'];
+  const runningStatus = [JOB_RUNNING_STATUS, JOB_IN_PROGRESS];
+  const errorStatus = [JOB_FAILED, JOB_STOPPED, JOB_INIT_FAILED, JOB_SYNC_FAILED, NODE_STATUS_OFFLINE, JOB_RESYNC_FAILED, JOB_INIT_SYNC_FAILED];
+  const progressArray = [JOB_ONGOING, JOB_INIT_PROGRESS, JOB_INIT_SYNC_PROGRESS, JOB_RESYNC_IN_PROGRESS, JOB_SYNC_IN_PROGRESS];
+
   if (!data) {
     return '-';
   }
@@ -16,9 +21,6 @@ function StatusItemRenderer({ data, field }) {
   if (resp === 'Partialycompleted') {
     resp = 'Partially Completed';
   }
-
-  // success info danger
-
   function statusRenderer({ name, title, text, space, icon }) {
     return (
       <div>
@@ -34,22 +36,30 @@ function StatusItemRenderer({ data, field }) {
       </div>
     );
   }
-  if (status === JOB_COMPLETION_STATUS || status === JOB_INIT_SUCCESS || status === NODE_STATUS_ONLINE || status === JOB_RESYNC_SUCCESS || status === JOB_IN_SYNC || status === 'migrated' || status === JOB_RECOVERED) {
-    return statusRenderer({ name: 'success' });
+  for (let i = 0; i < successArray.length; i += 1) {
+    if (status === successArray[i]) {
+      return statusRenderer({ name: 'success' });
+    }
   }
-  if (status === JOB_RUNNING_STATUS || status === JOB_IN_PROGRESS) {
-    return statusRenderer({ name: 'info', title: data.step, text: 'Running', space: true, icon: true });
+  for (let i = 0; i < runningStatus; i += 1) {
+    if (status === runningStatus[i]) {
+      return statusRenderer({ name: 'info', title: data.step, text: 'Running', space: true, icon: true });
+    }
   }
-  if (status === JOB_FAILED || status === JOB_STOPPED || status === JOB_INIT_FAILED || status === JOB_SYNC_FAILED || status === NODE_STATUS_OFFLINE || status === JOB_RESYNC_FAILED || status === JOB_INIT_SYNC_FAILED) {
-    const { failureMessage, errorMessage } = data;
-    const msg = (typeof failureMessage !== 'undefined' ? failureMessage : errorMessage);
-    return statusRenderer({ name: 'danger', title: msg });
+  for (let i = 0; i < errorStatus.length; i += 1) {
+    if (status === errorStatus[i]) {
+      const { failureMessage, errorMessage } = data;
+      const msg = (typeof failureMessage !== 'undefined' ? failureMessage : errorMessage);
+      return statusRenderer({ name: 'danger', title: msg });
+    }
   }
   if (status === JOB_ONGOING) {
     return statusRenderer({ name: 'info', space: true });
   }
-  if (status === JOB_ONGOING || status === JOB_INIT_PROGRESS || status === JOB_INIT_SYNC_PROGRESS || status === JOB_RESYNC_IN_PROGRESS || status === JOB_SYNC_IN_PROGRESS) {
-    return statusRenderer({ name: 'info', space: true, icon: true });
+  for (let i = 0; i < progressArray; i += 1) {
+    if (status === progressArray[i]) {
+      return statusRenderer({ name: 'info', space: true, icon: true });
+    }
   }
   if (status === JOB_COMPLETED_WITH_ERRORS) {
     return (
