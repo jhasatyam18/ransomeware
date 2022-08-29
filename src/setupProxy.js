6b5844a69f99d4db8dd5bdab75916449342e757f
shikha-max <tshikha66@gmail.com>
2022-08-29 19:30:54 +0530

    added comment in setup proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,17 +1,20 @@
-const { createProxyMiddleware, responseInterceptor } = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
     '/api/v1/*',
     createProxyMiddleware({
-      target: 'https://20.204.63.250:5000/', // https://18.221.189.216/ 3.218.156.149// https://34.71.172.8/ // 51.68.208.96
+      target: 'https://54.38.208.203:5000/', // https://18.221.189.216/ 3.218.156.149// https://34.71.172.8/ // 51.68.208.96
       changeOrigin: true,
       secure: false,
-      logLevel: 'debug',
-      onProxyRes: (proxyRes, req, res) => {
+    /** Below code is not used for debuging purposes 
+     * whenever we want to see which request is going from setupProxy below codes need to be uncommented
+     logLevel: 'debug',
+       onProxyRes: (proxyRes, req, res) => {
         const exchange = `[${req.method}] [${proxyRes.statusCode}] ${req.path} -> ${proxyRes.req.protocol}//${proxyRes.req.host}${proxyRes.req.path}`;
         console.log(exchange);
       }
+      */
     }),
   );
   app.use(
