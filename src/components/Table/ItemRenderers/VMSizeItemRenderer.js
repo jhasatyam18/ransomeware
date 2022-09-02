8376ab368fa0f4c1d7393449a97537dcd829fcf3
shikha-max <tshikha66@gmail.com>
2022-09-02 16:25:23 +0530

    added code to add size only if isDdeleted is false

--- src/components/Table/ItemRenderers/VMSizeItemRenderer.js
+++ src/components/Table/ItemRenderers/VMSizeItemRenderer.js
@@ -10,7 +10,9 @@ function VMSizeItemRenderer(props) {
   const { virtualDisks = [] } = data;
   if (virtualDisks !== null) {
     virtualDisks.forEach((disk) => {
-      size += disk.size;
+      if (!size.isDeleted) {
+        size += disk.size;
+      }
     });
   }
   size = convertKBtoUnit(size);
