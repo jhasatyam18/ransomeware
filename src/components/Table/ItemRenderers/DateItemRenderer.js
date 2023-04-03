f42342c0373fe503b5778af5e57ec05f092a03c5
shikha-max <tshikha66@gmail.com>
2023-04-03 18:40:33 +0530

    added lags by column in replication jobs

--- src/components/Table/ItemRenderers/DateItemRenderer.js
+++ src/components/Table/ItemRenderers/DateItemRenderer.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import 'boxicons';
-import { dateDiffInMonthDayFromnNow, dateDiffInHrMinSecFromNow } from '../../../utils/AppUtils';
 
 function DateItemRenderer({ data, field }) {
-  const fieldsForTimeDiff = ['currenSnapshotTime', 'lastSyncTime'];
   const time = data[field] * 1000;
   const d = new Date(time);
   let resp = '';
-  if (fieldsForTimeDiff.indexOf(field) !== -1) {
-    const dayDiff = dateDiffInMonthDayFromnNow(d);
-    const timeDiff = dateDiffInHrMinSecFromNow(d);
-    resp = `${dayDiff}  ${timeDiff}`;
-  } else {
-    resp = `${d.toLocaleDateString()}-${d.toLocaleTimeString()}`;
-  }
+  resp = `${d.toLocaleDateString()}-${d.toLocaleTimeString()}`;
   if (data[field] === 0) {
     resp = '-';
     return (
