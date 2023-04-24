b5819aac21b9ea682ba82b68096c03f79a8edeca
shikha-max <tshikha66@gmail.com>
2023-04-24 20:52:32 +0530

    moved business login to action files

--- src/components/Wizards/ReversePlanConfigStep.js
+++ src/components/Wizards/ReversePlanConfigStep.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { withTranslation } from 'react-i18next';
 import { Card, CardBody, CardTitle, Form } from 'reactstrap';
-import { PLATFORM_TYPES } from '../../constants/InputConstants';
 import { convertMinutesToDaysHourFormat } from '../../utils/AppUtils';
 import { getValue } from '../../utils/InputUtils';
 import DMField from '../Shared/DMField';
@@ -11,8 +10,6 @@ class ReversePlanConfigStep extends Component {
     const { user, dispatch, t } = this.props;
     const { values } = user;
     const drPlan = getValue('ui.reverse.drPlan', values);
-    const { protectedSite } = drPlan;
-    const { platformDetails } = protectedSite;
     let name = '';
     let replicationInterval = '';
     let protectedSiteName = '';
@@ -31,7 +28,7 @@ class ReversePlanConfigStep extends Component {
               <DMField dispatch={dispatch} user={user} fieldKey="reverse.name" text={name} />
               <DMField dispatch={dispatch} user={user} fieldKey="reverse.protectedSite" text={protectedSiteName} />
               <DMField dispatch={dispatch} user={user} fieldKey="reverse.recoverySite" />
-              {platformDetails.platformType !== PLATFORM_TYPES.VMware ? <DMField dispatch={dispatch} user={user} fieldKey="reverse.replType" /> : null}
+              <DMField dispatch={dispatch} user={user} fieldKey="reverse.replType" />
               <DMField dispatch={dispatch} user={user} fieldKey="reverse.suffix" />
               <DMField dispatch={dispatch} user={user} fieldKey="reverse.interval" text={interval} />
             </Form>
