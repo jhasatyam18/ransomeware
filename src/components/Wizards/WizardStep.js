2c9a296573577d70f619adb2d58430212c4af932
shikha-max <tshikha66@gmail.com>
2023-04-24 16:13:24 +0530

    removed differential reverse checkbox from pplan replication configuration step when vmware is target

--- src/components/Wizards/WizardStep.js
+++ src/components/Wizards/WizardStep.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import { CardBody, Form } from 'reactstrap';
+import { PLATFORM_TYPES } from '../../constants/InputConstants';
+import { getValue } from '../../utils/InputUtils';
 import DMField from '../Shared/DMField';
 
 class WizardStep extends Component {
   render() {
     const { dispatch, user, fields } = this.props;
+    const { values } = user;
+    const recoverySite = getValue('ui.values.recoveryPlatform', values) || '';
     if (!fields && fields.length <= 0) {
       return null;
     }
@@ -13,7 +17,7 @@ class WizardStep extends Component {
         <CardBody className="modal-card-body">
           <Form>
             {
-              fields.map((field) => (<DMField key={`${field}-DMField-key`} dispatch={dispatch} user={user} fieldKey={field} />))
+              fields.map((field) => (recoverySite === PLATFORM_TYPES.VMware && field === 'drplan.enableReverse' ? null : <DMField key={`${field}-DMField-key`} dispatch={dispatch} user={user} fieldKey={field} />))
             }
           </Form>
         </CardBody>
