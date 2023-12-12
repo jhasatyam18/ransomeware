fffe8db6b1140d1eda9b724c6b0944e7e3a65679
shikha-max <tshikha66@gmail.com>
2023-12-12 23:23:33 +0530

    added recovery configuration step for vmware-vmware

--- src/components/Wizards/RecoveryConfig.js
+++ src/components/Wizards/RecoveryConfig.js
@@ -13,59 +13,65 @@ class RecoveryConfig extends Component {
     const { values } = user;
     const workflow = getValue(STATIC_KEYS.UI_WORKFLOW, values);
     const recoveryPlatform = getValue('ui.values.recoveryPlatform', values);
+    const protectectionPlatform = getValue('ui.values.protectionPlatform', values);
     const option = getValue('recovery.discardPartialChanges', values);
-    const vmNotCompletedReplication = getValue('recovery.discard.warning.vms', values);
+    const vmNotCompletedReplication = getValue('recovery.discard.warning.vms', values) || [];
     const showOptionToDiscard = (workflow === UI_WORKFLOW.RECOVERY && vmNotCompletedReplication.length > 0 && recoveryPlatform === PLATFORM_TYPES.VMware);
+    const showInstallOption = protectectionPlatform !== recoveryPlatform && recoveryPlatform === PLATFORM_TYPES.VMware;
+
     const onChange = (value) => {
       dispatch(valueChange('recovery.discardPartialChanges', value));
     };
+
     const renderOptionToDiscard = () => (
       <>
         <div>
-          <input type="radio" id="cure" name="option" value="current" checked={!option} onChange={() => onChange(false)} />
+          <input type="radio" id="cure" name="option" value="current" checked={!option} disabled={!showOptionToDiscard} onChange={() => onChange(false)} />
           <label htmlFor="current" style={{ cursor: 'pointer', position: 'relative', top: '-2px', left: '5px' }} aria-hidden="true" onClick={() => onChange(false)}>{t('title.differntial.preserve.current')}</label>
         </div>
         <div>
-          <input type="radio" id="previous" name="option" value="previous" checked={option} onChange={() => onChange(true)} />
+          <input type="radio" id="previous" name="option" value="previous" disabled={!showOptionToDiscard} checked={option} onChange={() => onChange(true)} />
           <label htmlFor="previous" style={{ cursor: 'pointer', position: 'relative', top: '-2px', left: '5px' }} aria-hidden="true" onClick={() => onChange(true)}>{t('title.differential.discard.partial.changes')}</label>
-
         </div>
       </>
     );
 
-    const renderWarningText = () => (
-      <div className="margin-top-5 card_note_warning ">
-        <p className="rev_diff_warning">{t('reovery.discard.warning.txt')}</p>
-        { vmNotCompletedReplication.map((el) => <li style={{ width: '800px', paddingLeft: '22px', position: 'relative', top: '-10px' }}>{el}</li>)}
+    const renderWarningText = () => {
+      if (vmNotCompletedReplication.length === 0) {
+        return null;
+      }
+      return (
+        <div className="margin-top-5 card_note_warning ">
+          <p className="rev_diff_warning">{t('reovery.discard.warning.txt')}</p>
+          { vmNotCompletedReplication.map((el) => <li style={{ width: '800px', paddingLeft: '22px', position: 'relative', top: '-10px' }}>{el}</li>)}
+        </div>
+      );
+    };
 
-      </div>
-    );
     return (
       <>
+        {showInstallOption ? (
+          <Card className="padding-20">
+            <CardTitle>{t('Tools Installation')}</CardTitle>
+            <CardBody>
+              <Form className="form_w">
+                <DMField dispatch={dispatch} user={user} fieldKey="recovery.installSystemAgent" />
+                <DMField dispatch={dispatch} user={user} fieldKey="recovery.installCloudPkg" />
+                <DMField dispatch={dispatch} user={user} fieldKey="ui.installCloudPkg.warning" />
+                <DMField dispatch={dispatch} user={user} fieldKey="recovery.removeFromAD" />
+              </Form>
+            </CardBody>
+          </Card>
+        ) : null}
         <Card className="padding-20">
-          <CardTitle>{t('Tools Installation')}</CardTitle>
-          <CardBody>
+          <CardTitle>{t('Recovery Mode')}</CardTitle>
+          {renderWarningText()}
+          <CardBody className="recovey_warn_text ">
             <Form className="form_w">
-              <DMField dispatch={dispatch} user={user} fieldKey="recovery.installSystemAgent" />
-              <DMField dispatch={dispatch} user={user} fieldKey="recovery.installCloudPkg" />
-              <DMField dispatch={dispatch} user={user} fieldKey="ui.installCloudPkg.warning" />
-              <DMField dispatch={dispatch} user={user} fieldKey="recovery.removeFromAD" />
+              {renderOptionToDiscard()}
             </Form>
           </CardBody>
         </Card>
-        {
-            showOptionToDiscard ? (
-              <Card className="padding-20">
-                <CardTitle>{t('Recovery Mode')}</CardTitle>
-                {renderWarningText()}
-                <CardBody className="recovey_warn_text ">
-                  <Form className="form_w">
-                    {renderOptionToDiscard()}
-                  </Form>
-                </CardBody>
-              </Card>
-            ) : null
-          }
       </>
     );
   }
