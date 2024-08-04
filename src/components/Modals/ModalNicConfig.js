3525a15ea536fcdaf23edcf032d50c95c5afc606
Satyam Jha <149782181+jhasatyam18@users.noreply.github.com>
2024-08-04 20:07:38 +0530

    Main || Logo Change & Scripts is not visible in reverse wizard even if it is selected during plan creation (#1046)
    * Scripts is not visible in reverse wizard even if it is selected during plan creation (#1045)
    
    Issue: https://datamotiveplatform.atlassian.net/browse/ECM-691
    https://datamotiveplatform.atlassian.net/browse/ECM-690
    
    Testing:
    
    Edit the plan add scripts in plan and recovered that.
    Start the reverse and scripts are there present
    After reverse also scripts are present in VM as well as plan level
    In other iteration removed the scripts and that is removed from the plan
    Added warning message for script in reverse and it is not visible in other wizards
    
    * corrected tag name
    
    * Logo | Changed the logo in header (#1044)
    
    * changed the logo oof datamotive
    
    * left aligned the logo
    
    * Add warning message while taking action on alert also (#1048)
    
    * added warning message for adding new nic in vm
    * changed variable name
    
    * Script timeout is not getting update after reverse (#1050)
    
    * script timeout is not getting update after reverse
    * bood delay, start time, pre and post inputs is not getting update if user change during reverse
    
    * start time change during reverse

--- src/components/Modals/ModalNicConfig.js
+++ src/components/Modals/ModalNicConfig.js
@@ -54,7 +54,7 @@ class ModalNicConfig extends Component {
     const recoveryPlatform = getValue('ui.values.recoveryPlatform', values);
     const showPublicChk = index === 0;
     const workflow = getValue(STATIC_KEYS.UI_WORKFLOW, values);
-    const flow = (workflow !== UI_WORKFLOW.EDIT_PLAN && workflow !== UI_WORKFLOW.REVERSE_PLAN);
+    const flow = (workflow !== UI_WORKFLOW.EDIT_PLAN && workflow !== UI_WORKFLOW.REVERSE_PLAN && workflow !== UI_WORKFLOW.SINGLE_VM_EDIT);
     if (flow || !showPublicChk) {
       return null;
     }
