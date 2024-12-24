c6b70b5cc80f41eec5eb807797b28e882f8a3b58
Shikha Tiwari <84774237+shikha-max@users.noreply.github.com>
2024-12-24 21:10:38 +0530

    fix for https://datamotiveplatform.atlassian.net/browse/ECM-1707 (#1209)

--- DMSystem/src/Components/renderer/LinkRenderer.tsx
+++ DMSystem/src/Components/renderer/LinkRenderer.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { withTranslation, WithTranslation } from 'react-i18next';
-import { PORTS } from '../../Constants/userConstants';
-import { NodeInterface } from '../../interfaces/interfaces';
+import { PORTS, STATIC_KEYS } from '../../Constants/userConstants';
+import { NodeInterface, UserInterface } from '../../interfaces/interfaces';
+import { getValue } from '../../utils/inputUtils';
 
 interface Props extends WithTranslation {
     data: NodeInterface;
+    user: UserInterface;
 }
 
-const LinkRenderer: React.FC<Props> = ({ data, t }) => {
+const LinkRenderer: React.FC<Props> = ({ data, t, user }) => {
     const { hostname } = data;
+    const { values } = user;
+    const workflow = getValue(STATIC_KEYS.UI_CURRENT_FLOW, values);
     return (
         <a href={`https://${hostname}:${PORTS.FIVE_THOUSAND_FOUR}/upgrade`} key={`site-link-${hostname}-${data.id}`} target="_blank" rel="noopener noreferrer">
-            {t('click.to.upgrade')}
+            {workflow === STATIC_KEYS.UPGRADE ? t('click.to.upgrade') : t('click.to.revert')}
         </a>
     );
 };
